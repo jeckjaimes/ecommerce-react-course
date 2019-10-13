5cf43129970e9db638ccd42e220205720e6ac873
jeck jaimes <jeckjaimes@gmail.com>
2019-10-13 18:22:05 -0400

    some changes

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -35,8 +35,7 @@ const Header = ({ currentUser, hidden }) => (
             <CartIcon />
         </div>
         {
-            hidden ? null :
-            <CartDropdown />
+            hidden ? null : <CartDropdown />
         }
     </div>
 );
